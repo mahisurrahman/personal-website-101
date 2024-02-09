92630eb0d3022475d34615e2fc69894e6e018c5a
mahisurrahman <mahisur.rahman.001@gmail.com>
2024-02-10 00:23:23 +0600

    Completed the Contact Page

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBt9SBqIm9RLQ0tNDZJdIYa_phPGjJb_sg",
+  authDomain: "personal-project-101-df459.firebaseapp.com",
+  projectId: "personal-project-101-df459",
+  storageBucket: "personal-project-101-df459.appspot.com",
+  messagingSenderId: "629486625471",
+  appId: "1:629486625471:web:673c74d487fd3df1441c0a"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,10 +1,43 @@
+import { Fade } from "react-awesome-reveal";
+import "./Contact.css";
+import { Helmet } from "react-helmet-async";
 
 const Contact = () => {
-    return (
-        <div>
-            Contact Page
-        </div>
-    );
+  return (
+    <Fade cascade duration={1000}>
+      <Helmet>
+        <title>Mahisur | Contact</title>
+      </Helmet>
+      <div className="w-full py-[2vw] px-[5vw] flex justify-center items-center">
+        <form className="flex flex-col ">
+          <input
+            className="bg-transparent border border-blue-600 rounded-lg font-RussoOne text-[1.2vw] tracking-wider placeholder:text-yellow-500 w-[30vw] px-[2vw] py-[1vw]"
+            type="text"
+            name="name"
+            id=""
+            placeholder="Name"
+          />
+          <br />
+          <input
+            className="bg-transparent border border-blue-600 rounded-lg font-RussoOne text-[1.2vw] tracking-wider placeholder:text-yellow-500 w-[30vw] px-[2vw] py-[1vw]"
+            type="email"
+            name="email"
+            id=""
+            placeholder="Email"
+          />
+          <br />
+          <textarea
+            className="bg-transparent border border-blue-600 rounded-lg font-RussoOne text-[1.2vw] tracking-wider placeholder:text-yellow-500 w-[30vw] h-[15vw] px-[2vw] py-[1vw]"
+            type="text"
+            name="email"
+            id=""
+            placeholder="Text Here"
+          />
+          <input className="duration-700 w-[30vw] py-[0.25vw] bg-blue-700 mt-[1vw] rounded-2xl text-yellow-500 text-[2vw] font-RussoOne hover:bg-[#ff0000] hover:text-white hover:duration-700 hover:cursor-pointer" type="submit" value="Submit" />
+        </form>
+      </div>
+    </Fade>
+  );
 };
 
 export default Contact;
